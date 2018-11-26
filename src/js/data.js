30fca463385be46da87567ee6b745c8dadf2a8b5
Jennifer-Seery <jennifermarieseery15@gmail.com>
2018-11-26 20:54:13 +0000

    Changes made to pull data from an API

--- src/js/data.js
+++ src/js/data.js
@@ -1,14 +1,14 @@
-// Java Script Login Page
+const url = 'https://api.jsonbin.io/b/5bfc469299d3ae6554912e45/1';
+var Information = [];
 
-// Setting Variables
-const url = 'http://phorest-ob-staging.com/wp-json/wp/v2/posts/';
 const options= {method:null,headers:{name:null,value:null}};
 
-// Layout of the fetch function
 function fetch(url, options) 
 {
+  
+ 
   return new Promise(function(resolve, reject) 
-  {
+  { 
     var Req = new XMLHttpRequest();
     Req.open("GET", url); 
     Req.onload = function () {
@@ -26,13 +26,73 @@ function fetch(url, options)
 }
 
 
-// Function called after the login button is pressed.
-function check(form)
-{  
-  
-  // Fetch called 
-  fetch(url,options)
+
+fetch(url,options)
   .then(function(data)
-  {  
-	console.log(data);
-}}
+{
+    var toDo = [];
+  
+    for(var i=0;i<data.length;i++){
+      blogPosts=data;
+    }
+
+  for(var i=0;i<blogPosts.length;i++){
+    
+    if(i==0){
+      blogPost1Name=blogPosts[i].name;
+    //  blogPost1Desc=blogPosts[1].description;
+     
+    }
+    if(i==1){
+      blogPost2Name=blogPosts[i].name;
+   //   blogPost2Desc=blogPosts[2].description;
+      
+    }
+    if(i==2){
+      blogPost3Name=blogPosts[i].name;
+     // blogPost3Desc=blogPosts[3].description;
+      
+    }
+    if(i==3){
+      blogPost4Name=blogPosts[i].name;
+    //  blogPost4Desc=blogPosts[4].description;
+
+      
+    }
+  }
+
+  
+  document.getElementById('blog1').innerHTML=blogPost1Name;
+  document.getElementById('blog2').innerHTML=blogPost2Name;
+  document.getElementById('blog3').innerHTML=blogPost3Name;
+  document.getElementById('blog4').innerHTML=blogPost4Name;
+  
+  blogPost1Desc=blogPosts[0].description
+  blogPost2Desc=blogPosts[1].description
+  blogPost3Desc=blogPosts[2].description
+  blogPost4Desc=blogPosts[3].description
+
+  
+  document.getElementById('blog1Info').innerHTML=blogPost1Desc;
+  document.getElementById('blog2Info').innerHTML=blogPost2Desc;
+  document.getElementById('blog3Info').innerHTML=blogPost3Desc;
+  document.getElementById('blog4Info').innerHTML=blogPost4Desc;
+  
+
+  
+
+})
+
+  
+  
+  
+  .catch(function(err)
+{
+  console.error(err);  
+});
+      
+
+
+
+
+
